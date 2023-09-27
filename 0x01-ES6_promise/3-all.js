db2a44fefd6179017eb4bf21491fe2158ba85079
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-27 22:24:38 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,23 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  const promises = [];
-
-  // Create an array of promises
-  promises.push(uploadPhoto());
-  promises.push(createUser());
-
-  // Use Promise.all to collectively resolve all promises
-  Promise.all(promises)
-    .then((results) => {
-      results.forEach((result) => {
-        const { body, firstName, lastName } = result;
-        console.log(`${body} ${firstName} ${lastName}`);
-      });
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
