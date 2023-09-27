12d18765415cd4762dacfa0e95ef11055b1ecede
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-27 22:59:06 +0300

    9-try.js comments

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,20 @@
+// Define the guardrail function that accepts a mathFunction
 export default function guardrail(mathFunction) {
+  // Initialize an empty array called "queue" to store results and messages
   const queue = [];
 
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.message);
+    // Try to execute the mathFunction and push its result to the queue
+    queue.push(mathFunction());
+  } catch (err) {
+    // If an error occurs during execution, push the error message
+    //  (converted to a string) to the queue
+    queue.push(String(err));
   } finally {
+    // Regardless of success or error, push the "Guardrail was processed" message to the queue
     queue.push('Guardrail was processed');
   }
 
+  // Return the queue containing results and messages
   return queue;
 }
