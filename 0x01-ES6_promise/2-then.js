825b396adf6a22848efcc50774e290ed23d535c7
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-27 22:14:19 +0300

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,14 +2,8 @@
 export default function handleResponseFromAPI(promise) {
   // Append three handlers to the promise
   return promise
-    .then(() => {
-      // Return an object when the promise resolves
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      // Return an empty error object when the promise rejects
-      return new Error();
-    })
+    .then(() => ({ status: 200, body: 'success' })) // Parenthesize and move the returned object
+    .catch(() => new Error()) // Parenthesize and move the returned object
     .finally(() => {
       // Log a message to the console for every resolution
       console.log('Got a response from the API');
