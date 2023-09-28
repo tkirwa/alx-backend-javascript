3838c337499fcc432210eab6f31ad1a501318777
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-28 23:58:05 +0300

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,27 +1,25 @@
-/* eslint-disable no-underscore-dangle */
 // 10-car.js
-
-// Import the Symbol class from the built-in module
-import { Symbol } from 'symbol';
-
-// Define a private property using a symbol
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
 
 // Define the Car class
 export default class Car {
-  // Define the constructor with the attributes
-  constructor(brand, motor, color) {
-    // Assign the attributes to the private properties
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    // Define the constructor with the attributes
+    constructor(brand, motor, color) {
+      // Assign the attributes to the instance properties
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+    // Define a static getter for the Symbol.species property
+    // This returns the constructor of the class that created the instance
+    static get Symbol.species {
+      return this.prototype.constructor;
+    }
+  
+    // Define the cloneCar method
+    cloneCar() {
+      // Return a new object of the same class with undefined properties
+      // Use the Symbol.species property to get the correct constructor
+      return new (this.constructor[Symbol.species])();
+    }
   }
-
-  // Define the cloneCar method
-  cloneCar() {
-    // Return a new object of the same class with undefined properties
-    return new this.constructor();
-  }
-}
