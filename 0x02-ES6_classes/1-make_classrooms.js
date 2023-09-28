bcc101f33d990955eef5176d84f8a6975e540cfc
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-28 22:29:17 +0300

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,12 @@
-// 1-make_classrooms.js
+// Import the ClassRoom class from the '0-classroom' module
+import ClassRoom from "./0-classroom";
 
-import ClassRoom from './0-classroom.js';
-
+// Define the initializeRooms function
 export default function initializeRooms() {
-  // Create an array of ClassRoom objects with specified sizes
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  // Create and return an array of ClassRoom objects with specific sizes
+  return [
+    new ClassRoom(19), // Create a ClassRoom object with size 19
+    new ClassRoom(20), // Create a ClassRoom object with size 20
+    new ClassRoom(34), // Create a ClassRoom object with size 34
+  ];
 }
