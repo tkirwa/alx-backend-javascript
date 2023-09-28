0bc43c92dd87991ec35ec3dd67fc05d445b46d37
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-28 23:07:37 +0300

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,19 @@
-// 5-building.js
-
-// Define the Building class as an abstract class
+// Define the Building class
 export default class Building {
-  // Constructor to initialize the class attributes
   constructor(sqft) {
-    this.sqft = sqft; // Set the sqft attribute
+    // Check if the constructor is called directly on the Building class
+    if (this.constructor !== Building) {
+      // Get the property names of the constructor's prototype
+      const props = Object.getOwnPropertyNames(this.constructor.prototype);
+      // Check if there is no 'evacuationWarningMessage' method in the prototype
+      if (!props.find((e) => e === 'evacuationWarningMessage')) {
+        // Throw an error if the subclass does not override 'evacuationWarningMessage'
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage'
+        );
+      }
+    }
+    this._sqft = sqft; // Set the sqft attribute with an underscore
   }
 
   // Getter for the sqft attribute
@@ -14,17 +23,10 @@ export default class Building {
 
   // Setter for the sqft attribute
   set sqft(sqft) {
-    // Check if the provided sqft is a number or an instance of Number
+    // Check if the provided sqft is not a number or an instance of Number
     if (typeof sqft !== 'number' && !(sqft instanceof Number)) {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft; // Set the private _sqft attribute
-  }
-
-  // Abstract method that must be implemented by subclasses
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage'
-    );
   }
 }
