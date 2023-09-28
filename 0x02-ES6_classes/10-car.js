4017aa6009a6ba120a8f620cca6c3e4f22fc7c44
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-28 23:59:47 +0300

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,25 +1,43 @@
-// 10-car.js
-
-// Define the Car class
+/* eslint-disable no-underscore-dangle */
 export default class Car {
-    // Define the constructor with the attributes
-    constructor(brand, motor, color) {
-      // Assign the attributes to the instance properties
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    // Define a static getter for the Symbol.species property
-    // This returns the constructor of the class that created the instance
-    static get Symbol.species {
-      return this.prototype.constructor;
-    }
-  
-    // Define the cloneCar method
-    cloneCar() {
-      // Return a new object of the same class with undefined properties
-      // Use the Symbol.species property to get the correct constructor
-      return new (this.constructor[Symbol.species])();
-    }
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
