38349bea086cc47fbaa8cdebd548471abcffea15
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-29 00:03:38 +0300

    10-car.js --fix

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,43 +1,63 @@
-/* eslint-disable no-underscore-dangle */
+// 10-car.js
+
+// Define the Car class
 export default class Car {
-  constructor(brand, motor, color) {
-    this.brand = brand;
-    this.motor = motor;
-    this.color = color;
+    // Define the constructor with the attributes
+    constructor(brand, motor, color) {
+      // Use getters and setters to access the private properties
+      this.brand = brand;
+      this.motor = motor;
+      this.color = color;
+    }
+  
+    // Define a getter for the brand property
+    get brand() {
+      // Return the value of the private property _brand
+      return this._brand;
+    }
+  
+    // Define a setter for the brand property
+    set brand(value) {
+      // Assign the value to the private property _brand
+      this._brand = value;
+    }
+  
+    // Define a getter for the motor property
+    get motor() {
+      // Return the value of the private property _motor
+      return this._motor;
+    }
+  
+    // Define a setter for the motor property
+    set motor(value) {
+      // Assign the value to the private property _motor
+      this._motor = value;
+    }
+  
+    // Define a getter for the color property
+    get color() {
+      // Return the value of the private property _color
+      return this._color;
+    }
+  
+    // Define a setter for the color property
+    set color(value) {
+      // Assign the value to the private property _color
+      this._color = value;
+    }
+  
+    // Define a static getter for the Symbol.species property
+    // This returns the constructor of the class that created the instance
+    static get Symbol.species {
+      return this;
+    }
+  
+    // Define the cloneCar method
+    cloneCar() {
+      // Get the constructor of the class using Symbol.species
+      const Species = this.constructor[Symbol.species];
+      // Return a new object of the same class with undefined properties
+      return new Species();
+    }
   }
-
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(value) {
-    this._brand = value;
-  }
-
-  get motor() {
-    // eslint-disable-next-line no-underscore-dangle
-    return this._motor;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value) {
-    this._color = value;
-  }
-
-  static get [Symbol.species]() {
-    return this;
-  }
-
-  cloneCar() {
-    const Species = this.constructor[Symbol.species];
-
-    return new Species();
-  }
-}
+  
