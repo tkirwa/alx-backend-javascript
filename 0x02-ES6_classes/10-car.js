597cc2c58a232c792e1e41d997a065e4f839bc52
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-29 00:05:56 +0300

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,63 +1,52 @@
-// 10-car.js
+/* eslint-disable no-underscore-dangle */
+// Disable eslint warnings for underscores in property names
 
-// Define the Car class
 export default class Car {
-    // Define the constructor with the attributes
-    constructor(brand, motor, color) {
-      // Use getters and setters to access the private properties
-      this.brand = brand;
-      this.motor = motor;
-      this.color = color;
-    }
-  
-    // Define a getter for the brand property
-    get brand() {
-      // Return the value of the private property _brand
-      return this._brand;
-    }
-  
-    // Define a setter for the brand property
-    set brand(value) {
-      // Assign the value to the private property _brand
-      this._brand = value;
-    }
-  
-    // Define a getter for the motor property
-    get motor() {
-      // Return the value of the private property _motor
-      return this._motor;
-    }
-  
-    // Define a setter for the motor property
-    set motor(value) {
-      // Assign the value to the private property _motor
-      this._motor = value;
-    }
-  
-    // Define a getter for the color property
-    get color() {
-      // Return the value of the private property _color
-      return this._color;
-    }
-  
-    // Define a setter for the color property
-    set color(value) {
-      // Assign the value to the private property _color
-      this._color = value;
-    }
-  
-    // Define a static getter for the Symbol.species property
-    // This returns the constructor of the class that created the instance
-    static get Symbol.species {
-      return this;
-    }
-  
-    // Define the cloneCar method
-    cloneCar() {
-      // Get the constructor of the class using Symbol.species
-      const Species = this.constructor[Symbol.species];
-      // Return a new object of the same class with undefined properties
-      return new Species();
-    }
+  constructor(brand, motor, color) {
+    // Constructor for the Car class
+    this.brand = brand; // Public property 'brand'
+    this.motor = motor; // Public property 'motor'
+    this.color = color; // Public property 'color'
   }
-  
+
+  // Getters and setters for 'brand', 'motor', and 'color' properties
+  // These use internal underscored variables to store the values
+
+  get brand() {
+    return this._brand; // Getter for 'brand' property
+  }
+
+  set brand(value) {
+    this._brand = value; // Setter for 'brand' property
+  }
+
+  get motor() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._motor; // Getter for 'motor' property
+  }
+
+  set motor(value) {
+    this._motor = value; // Setter for 'motor' property
+  }
+
+  get color() {
+    return this._color; // Getter for 'color' property
+  }
+
+  set color(value) {
+    this._color = value; // Setter for 'color' property
+  }
+
+  static get [Symbol.species]() {
+    // Static getter for the Symbol.species property
+    // Returns the class itself, which is used in cloneCar()
+    return this;
+  }
+
+  cloneCar() {
+    // Method to create a new instance of the same class
+    const Species = this.constructor[Symbol.species]; // Get the class using Symbol.species
+
+    return new Species();
+  }
+}
