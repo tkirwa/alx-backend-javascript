f21948419b1a5f1bc882590f61e6b55133d7d1c6
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-03 10:14:12 +0300

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,17 @@
+// 0-get_list_students.js
+
+// Define the function getListStudents
+function getListStudents() {
+  // Create an array of student objects
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  // Return the array of students
+  return students;
+}
+
+// Export the getListStudents function
+export default getListStudents;
