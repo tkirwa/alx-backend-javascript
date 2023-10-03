f26804178eecd2a7ca556ab1f7afe59cd54afdbc
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-03 22:05:58 +0300

    1. More mapping - 1 - get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,17 +1,15 @@
 // 1-get_list_student_ids.js
 
 // Define the function getListStudentIds
-function getListStudentIds(arrayOfStudents) {
+const getListStudentIds = (arrayOfStudents) => {
   // Check if the input is an array
   if (!Array.isArray(arrayOfStudents)) {
     return [];
   }
-
   // Use map to extract the IDs and return them in a new array
   const studentIds = arrayOfStudents.map((student) => student.id);
 
   return studentIds;
-}
+};
 
-// Export the getListStudentIds function
 export default getListStudentIds;
