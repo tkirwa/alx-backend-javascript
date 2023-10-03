e01ed7a48dd6c092f46ea08497c78a9f4012c3db
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-03 22:54:56 +0300

    3-get_ids_sum.js - Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,12 +1,12 @@
 // 3-get_ids_sum.js
 
 // Define the function getStudentIdsSum
-function getStudentIdsSum(students) {
+const getStudentIdsSum = (students) => {
   // Use reduce to calculate the sum of student IDs
   const sumOfIds = students.reduce((acc, student) => acc + student.id, 0);
 
   return sumOfIds;
-}
+};
 
 // Export the getStudentIdsSum function
 export default getStudentIdsSum;
