806ada5a7ef5e8180c0c7d4551c8aafd8b7936f0
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-03 23:32:19 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,13 @@
+/* eslint-disable quotes */
 // 8-clean_set.js
 
 // Define the function cleanSet
 function cleanSet(set, startString) {
-  // Initialize an empty result string
-  let result = '';
+  // Use the filter method to get values that start with startString
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
 
-  // Iterate over the set values
-  for (const value of set) {
-    // Check if the value starts with the specified startString
-    if (value.startsWith(startString)) {
-      // Append the rest of the string to the result
-      result += value.slice(startString.length) + '-';
-    }
-  }
-
-  // Remove the trailing '-' and return the result
-  return result.slice(0, -1);
+  // Join the filtered values into a single string separated by '-'
+  return filteredValues.join("-");
 }
 
 // Export the cleanSet function
