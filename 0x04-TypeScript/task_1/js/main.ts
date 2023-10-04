fc61907126715bf27df39e9ce7b381ffcd453ed2
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-04 22:21:43 +0300

    task_1/js/main.ts - extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,22 +2,35 @@
 
 // Define the Teacher interface
 interface Teacher {
-    firstName: string;
-    lastName: string;
-    readonly fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    readonly location: string;
-    [key: string]: any; // Allow additional attributes of any type
-  }
-  
-  // Create a Teacher object
-  const teacher3: Teacher = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullTimeEmployee: false,
-    location: 'London',
-    contract: false, // Additional attribute
-  };
-  
-  console.log(teacher3);
-  
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; // Allow additional attributes of any type
+}
+
+// Create a Teacher object
+const teacher3: Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: false,
+  location: "London",
+  contract: false, // Additional attribute
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'London',
+  numberOfReports: 17,
+};
+
+console.log(director1);
