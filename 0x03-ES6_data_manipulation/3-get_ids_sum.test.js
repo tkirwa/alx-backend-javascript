4c8a7097f2229048c5cbf071d5fa58bca39dddda
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-04 21:44:19 +0300

    3-get_ids_sum.test.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.test.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.test.js
@@ -1,14 +1,10 @@
-import getStudentIdsSum from './3-get_ids_sum';
+import getStudentIdsSum from "./3-get_ids_sum";
 
 // Sample array of students for testing
-const students = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 },
-];
+const students = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
 // Test the getStudentIdsSum function
-test('getStudentIdsSum should return the sum of student IDs', () => {
+test("getStudentIdsSum should return the sum of student IDs", () => {
   const sum = getStudentIdsSum(students);
   expect(sum).toBe(6); // Adjust the expected value based on your data
 });
