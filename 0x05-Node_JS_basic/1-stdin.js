79e36e37c8fa16432261095edf06c1fd543bbd40
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-21 22:54:44 +0300

    update Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,20 @@
-process.stdin.setEncoding('utf8');
+// Write a welcome message to the console
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?');
-
+// Listen for user input
 process.stdin.on('readable', () => {
-  let chunk;
-  while ((chunk = process.stdin.read()) !== null) {
-    console.log(`Your name is: ${chunk}`);
+  // Read the user's input
+  const chunk = process.stdin.read();
+
+  // If the user provided input (i.e., the input is not null)
+  if (chunk) {
+    // Write the user's name to the console
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
+// Listen for the end of the input stream (i.e., when the user stops providing input)
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-  process.exit();
+  // Write a closing message to the console
+  process.stdout.write('This important software is now closing\n');
 });
