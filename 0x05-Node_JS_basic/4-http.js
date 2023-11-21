493b6bbe8b6603480eb8572fd7f2f35288d76396
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-21 23:22:52 +0300

    use single quotes

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
-const http = require("http");
+const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello Holberton School!");
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
