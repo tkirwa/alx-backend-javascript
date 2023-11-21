3800fb11ece7c9f914760e57abf424e50b5a28fd
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-21 23:28:46 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,55 +1,24 @@
-// Importing necessary modules
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-// Read the database file
-const databaseFile = process.argv[2];
-const databaseContent = fs.readFileSync(databaseFile, "utf8").split("\n");
-
-// Create HTTP server
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-
-  // Handling requests based on URL path
-  if (req.url === "/") {
-    res.end("Hello Holberton School!\n");
-  } else if (req.url === "/students") {
-    // Filtering out empty lines from the database content
-    const students = databaseContent.filter((line) => line.trim() !== "");
-
-    // Displaying the list of students
-    res.write("This is the list of our students\n");
-    res.write(`Number of students: ${students.length}\n`);
-
-    const csStudents = students.filter(
-      (student) => student.split(",")[3] === "CS"
-    );
-    const sweStudents = students.filter(
-      (student) => student.split(",")[3] === "SWE"
-    );
-
-    res.write(
-      `Number of students in CS: ${csStudents.length}. List: ${csStudents
-        .map((student) => student.split(",")[0])
-        .join(", ")}\n`
-    );
-    res.write(
-      `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents
-        .map((student) => student.split(",")[0])
-        .join(", ")}\n`
-    );
-
-    res.end();
-  } else {
-    // If the URL path is not recognized
-    res.statusCode = 404;
-    res.end("Not Found\n");
-  }
+const app = http.createServer(async (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    if (req.url === '/') {
+        res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+        try {
+            await countStudents(process.argv[2]);
+            res.end();
+        } catch (error) {
+            res.end(error.message);
+        }
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
-// Listening on port 1245
 app.listen(1245);
 
-// Exporting the app
 module.exports = app;
