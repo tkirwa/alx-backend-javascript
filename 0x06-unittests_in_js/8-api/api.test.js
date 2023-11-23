c89a977a9c0a05a89e3e4ed75d7ca2e6effd2772
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-24 01:09:21 +0300

    9. Regex integration testing - 9-api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -16,22 +16,4 @@ describe("Index page", function () {
       done();
     });
   });
-
-  it("should return correct status code and message when id is a number", function (done) {
-    request("http://localhost:7865/cart/12", function (error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal("Payment methods for cart 12");
-      done();
-    });
-  });
-
-  it("should return 404 status code when id is not a number", function (done) {
-    request(
-      "http://localhost:7865/cart/hello",
-      function (error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
-      }
-    );
-  });
 });
