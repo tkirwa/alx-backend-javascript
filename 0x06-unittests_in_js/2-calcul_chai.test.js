0fa95d3d9e27234e6037e8390f63882f60f1d475
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-23 23:12:17 +0300

    2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,24 +1,25 @@
 /* global describe it */
-const assert = require("assert");
-const calculateNumber = require("./1-calcul.js");
+const chai = require("chai");
+const expect = chai.expect;
+const calculateNumber = require("./2-calcul_chai.js");
 
 describe("calculateNumber", function () {
   describe("SUM", function () {
     it("should return 6 when inputs are 1.4 and 4.5", function () {
-      assert.equal(calculateNumber("SUM", 1.4, 4.5), 6);
+      expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
     });
   });
   describe("SUBTRACT", function () {
     it("should return -4 when inputs are 1.4 and 4.5", function () {
-      assert.equal(calculateNumber("SUBTRACT", 1.4, 4.5), -4);
+      expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
     });
   });
   describe("DIVIDE", function () {
     it("should return 0.2 when inputs are 1.4 and 4.5", function () {
-      assert.equal(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
+      expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
     });
     it("should return Error when second input is 0", function () {
-      assert.equal(calculateNumber("DIVIDE", 1.4, 0), "Error");
+      expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
     });
   });
 });
